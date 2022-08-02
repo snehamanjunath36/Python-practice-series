8e38055e0279bc4eb7cb77a90734263bab8144d7
sneha <smgsneha14@gmail.com>
2022-08-03 01:20:05 +0530

    6th prog

---
+++ swap_two_numbers.py
@@ -0,0 +1,5 @@
+n1 = input("Insert the 1st number to be swaped: ")
+n2 = input("Insert the 2nd number to be swaped: ")
+n1, n2 = n2, n1
+print(f"""\nThe inserted order was: ({n2}, {n1}) 
+After swaping the order is: ({n1}, {n2})""")
